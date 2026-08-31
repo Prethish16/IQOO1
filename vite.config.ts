14b3fb4ef7b5596cc4c2d2f090ef157dffc5cfd9
Prethish16 <prethish.s2024@vitstudent.ac.in>
2026-08-31 18:56:39 +0530

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/IQOO1/',
   plugins: [react()],
   server: { port: 8000, host: '0.0.0.0' }
 })
